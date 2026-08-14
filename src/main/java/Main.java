4130bc83d4e09dc17138b95bd6e6243b01f43286
Flavio Filho <fpneu911@gmail.com>
2026-08-14 16:18:37 -0300

    criação de Usuario e teste de fluxo criando um usuario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 package main.java;
 
+import main.java.model.Usuario;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sistema de Biblioteca");
+
+        Usuario usuario = new Usuario(1, "Flavio", "flavio@email.com");
+        System.out.println(usuario.getNome());
+        System.out.println(usuario.getEmail());
     }
 }
