6c1d8b3c10e93f481da21d6ce50ab6f019da67b1
Flavio Filho <fpneu911@gmail.com>
2026-08-14 16:41:36 -0300

    Criação classe emprestimo e teste na main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ package main.java;
 
 import main.java.model.Usuario;
 import main.java.model.Livro;
+import java.time.LocalDate;
+import main.java.model.Emprestimo;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +18,21 @@ public class Main {
          2008,
          5
         );
+
+        Emprestimo emprestimo = new Emprestimo(
+        1,
+        usuario,
+        livro,
+        LocalDate.now(),
+        LocalDate.now().plusDays(7)
+        );
+
+        System.out.println();
+        System.out.println("Empréstimo:");
+        System.out.println("Usuário: " + emprestimo.getUsuario().getNome());
+        System.out.println("Livro: " + emprestimo.getLivro().getTitulo());
+        System.out.println("Data do empréstimo: " + emprestimo.getDataEmprestimo());
+        System.out.println("Data de devolução: " + emprestimo.getDataDevolucao());
 
         System.out.println("Usuário: " + usuario.getNome());
         System.out.println("Email: " + usuario.getEmail());
