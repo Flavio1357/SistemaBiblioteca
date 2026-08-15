379cb7845b5ac3e31816fe3397825f5907333d80
Flavio Filho <fpneu911@gmail.com>
2026-08-14 22:01:01 -0300

    Melhorando encapsulamneto

--- src/main/java/service/BibliotecaService.java
+++ src/main/java/service/BibliotecaService.java
@@ -22,24 +22,12 @@ public class BibliotecaService {
         return usuarios;
     }
 
-    public void setUsuarios(ArrayList<Usuario> usuarios) {
-        this.usuarios = usuarios;
-    }
-
     public ArrayList<Livro> getLivros() {
         return livros;
     }
 
-    public void setLivros(ArrayList<Livro> livros) {
-        this.livros = livros;
-    }
-
     public ArrayList<Emprestimo> getEmprestimos() {
         return emprestimos;
-    }
-
-    public void setEmprestimos(ArrayList<Emprestimo> emprestimos) {
-        this.emprestimos = emprestimos;
     }
 
     public void cadastrarUsuario(Usuario usuario) {
